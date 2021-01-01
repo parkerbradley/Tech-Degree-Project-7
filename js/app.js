b60e9c67d32ac765b2060ed96483f5d58c3ae6f7
parkerbradley <parkerbradley93@gmail.com>
2021-01-01 17:27:02 -0600

    final edits

--- js/app.js
+++ js/app.js
@@ -9,3 +9,22 @@ close.addEventListener('click', (e) => {
   alert.style.display = 'none';
   notified.style.display = 'none';
 });
+
+
+//----------- Message User Functionality -----------//
+const userSearch = document.getElementById('search-for-user');
+const userMessage = document.getElementById('message-for-user');
+const errorMessage = document.getElementById('error');
+const messageButton = document.getElementById('message-button');
+const names = document.getElementsByClassName('name');
+
+
+messageButton.addEventListener('click', (e) => {
+  if (userSearch.value.length == 0 | userMessage.value.length == 0) {
+    errorMessage.style.display = 'block';
+  } else {
+    userSearch.value = '';
+    userMessage.value = '';
+    window.alert('Your Message Was Successfully Sent!');
+  };
+});
